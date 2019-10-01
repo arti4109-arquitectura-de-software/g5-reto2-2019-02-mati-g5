eff0ed4f1600a3cd60eafbf57ffad5aac7f0028a
fredy <fredy88x@yahoo.es>
2019-09-30 20:28:58 -0500

    codigo

---
+++ Experimento_Seguridad/atpos/src/main/java/atpos/FacturaBean.java
@@ -0,0 +1,13 @@
+package atpos;
+
+import javax.annotation.ManagedBean;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class FacturaBean {
+
+	
+	
+}
